582576d65948fe20c7901825d4185c6d97f7bfd2
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-02 15:50:13 -0700

    add question vars and arithmaic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
-  $("#form-one").submit(function() {
-  });
-
-  $("#question1").val();
-  $("#question2").val();
-  $("#question3").val();
-  $("#question4").val();
-  $("#question5").val();
-  $("#question6").val();
-  $("#question7").val();
-  $("#question8").val();
+//  $("#form-one").submit(function() {
 
 
+  var q1 = parseInt($("#question1").val());
+  var q2 = parseInt($("#question2").val());
+  var q3 = parseInt($("#question3").val());
+  var q4 = parseInt($("#question4").val());
+  var q5 = parseInt($("#question5").val());
+  var q6 = parseInt($("#question6").val());
+  var q7 = parseInt($("#question7").val());
+  var q8 = parseInt($("#question8").val());
+
+function add (q1, q2, q3, q4, q5, q6, q7, q8) {
+  return q1 + q2 + q3+ q4 + q5 + q6 + q7 + q8
+}
+
+
+  if (q1 >= 1) {
+    $("#java").show();
 
 
 });
@@ -24,10 +30,12 @@ $(document).ready(function() {
 
 
 
-
-
-
-
+//  $('select').change(function(){
+//      var sum = 0;
+//      $('select :selected').each(function() {
+//          sum += Number($(this).val());
+//      });
+//       $("#sum").html(sum);
 
 
 
