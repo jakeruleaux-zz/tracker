b743bdaf02002805e02a86858b84955571f381b1
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-02 09:55:28 -0700

    add var for qestions to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $("#form-one").submit(function() {
-    
-  }
+    var question1 = $("#question1")val();
+    var question2 = $("#question2")val();
+    var question3 = $("#question3")val();
+    var question4 = $("#question4")val();
+    var question5 = $("#question5")val();
+    var question6 = $("#question6")val();
+    var question7 = $("#question7")val();
+    var question8 = $("#question8")val();
+
 });
