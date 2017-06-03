aa406bb4d350b745889ace97ad718af2d5013e3d
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-03 08:41:38 -0700

    repair select box default and js branching ranges

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
   var q8 = parseInt($("#question8").val());
   var total = q1 + q2 + q3+ q4 + q5 + q6 + q7 + q8;
 
-  if (total <= 9) {
+  if (total <= 10) {
     $("#php").show();
-  } else if (total <= 16) {
+  } else if (total <= 17 && >= 13) {
     $("#ruby").show();
-  } else if (total <= 24) {
+  } else if (total <= 26 && >= 20) {
     $("#c-sharp").show();
-  } else (total >= 32)
+  } else (total === 32 && >= 27)
     $("#java").show();
   });
 $("#form-one").submit(function() {
