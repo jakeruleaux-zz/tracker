0d74894d436e8717467bcd8c7a9f8250b1201d3d
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-09 07:44:04 -0700

    adjust java branching format and add col and row to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
-  var q1 = parseInt($("#question1").val());
-  var q2 = parseInt($("#question2").val());
-  var q3 = parseInt($("#question3").val());
-  var q4 = parseInt($("#question4").val());
-  var q5 = parseInt($("#question5").val());
-  var q6 = parseInt($("#question6").val());
-  var q7 = parseInt($("#question7").val());
-  var q8 = parseInt($("#question8").val());
-  var total = q1 + q2 + q3+ q4 + q5 + q6 + q7 + q8;
+  $("#form-one").submit(function(event) {
+    event.preventDefault();
+      var q1 = parseInt($("#question1").val());
+      var q2 = parseInt($("#question2").val());
+      var q3 = parseInt($("#question3").val());
+      var q4 = parseInt($("#question4").val());
+      var q5 = parseInt($("#question5").val());
+      var q6 = parseInt($("#question6").val());
+      var q7 = parseInt($("#question7").val());
+      var q8 = parseInt($("#question8").val());
+      var total = q1 + q2 + q3+ q4 + q5 + q6 + q7 + q8;
 
-  if (total <= 10) {
-    $("#php").show();
-  } else if (total <= 17 && >= 13) {
-    $("#ruby").show();
-  } else if (total <= 26 && >= 20) {
-    $("#c-sharp").show();
-  } else (total === 32 && >= 27)
-    $("#java").show();
+      if ((total === 8) && (total <=12 )) {
+        $("#php").show();
+      } else if ((total === 13) && (total <= 18)) {
+        $("#ruby").show();
+      } else if ((total === 19) && (total <= 26)) {
+        $("#c-sharp").show();
+      } else
+      $("#java").show();
   });
-$("#form-one").submit(function() {
  });
 
 
