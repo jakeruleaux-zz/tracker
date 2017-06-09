ccf99993b6e3ab0c8f42ced0b3a7f7af8ec77e74
jacob ruleaux <jakeruleaux@gmail.com>
2017-06-09 16:44:22 -0700

    debugs some of js

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,26 @@ $(document).ready(function() {
       var q7 = parseInt($("#question7").val());
       var q8 = parseInt($("#question8").val());
       var total = q1 + q2 + q3+ q4 + q5 + q6 + q7 + q8;
-console.log(total);
-      if ((total === 8) && (total <=12 )) {
+// console.log(total);
+      if ((total >= 8) && (total <=12 )) {
         $("#php").show();
-      } else if ((total === 13) && (total <= 18)) {
+        $("#ruby").hide();
+        $("#c-sharp").hide();
+        $("#java").hide();
+      } else if ((total >= 13) && (total <= 18)) {
         $("#ruby").show();
-      } else if ((total === 19) && (total <= 26)) {
+        $("#c-sharp").hide();
+        $("#java").hide();
+        $("#php").hide();
+      } else if ((total >= 19) && (total <= 26)) {
         $("#c-sharp").show();
+        $("#java").hide();
+        $("#php").hide();
+        $("#ruby").hide();
       } else
-      $("#java").show();
+        $("#java").show();  
+        $("#ruby").hide();
+        $("#php").hide();
+        $("#c-sharp").hide();
   });
  });
